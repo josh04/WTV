bf4bc713e2aa75b120032618aec8556a6de69ba6
Josh04 <josh04@gmail.com>
2011-10-26 16:58:34 +0000

    Midway through add entry menu

--- apache/django.wsgi
+++ apache/django.wsgi
@@ -2,6 +2,6 @@ import os, sys
 sys.path.append('/home/django')
 sys.path.append('/home/django/wtv-dev')
 sys.path.append('/usr/local/lib/python2.6/dist-packages/appmedia')
-os.environ['DJANGO_SETTINGS_MODULE'] = 'wtv.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'wtv-dev.settings'
 import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()
--- urls.py
+++ urls.py
@@ -10,8 +10,9 @@ urlpatterns = patterns('',
     url(r'^comments/', include('django.contrib.comments.urls')),
     (r'^polls/', include('polls.urls')),
     (r'^booking/', include('booking.urls')),
+    url(r'^add/', 'wtv-dev.views.wtv_entry_add.view_wtv_entry_add'),
+    (r'^accounts/login/$', 'django.contrib.auth.views.login'),
     url(r'^', include('cms.urls')),
-
 )
 
 if settings.DEBUG:
